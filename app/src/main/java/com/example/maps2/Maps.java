afd6f1cc3438871e3aec6353bfb72fbfa5798aff
Gabriel Davila <gabrielsdavila2@gmail.com>
2025-04-04 08:48:09 -0300

    teste com poligonos

--- app/src/main/java/com/example/maps2/Maps.java
+++ app/src/main/java/com/example/maps2/Maps.java
@@ -18,8 +18,18 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Dash;
+import com.google.android.gms.maps.model.Dot;
+import com.google.android.gms.maps.model.Gap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PatternItem;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class Maps extends AppCompatActivity implements OnMapReadyCallback {
     SeekBar seekBar;
@@ -73,7 +83,92 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        //abrir em rolante
         LatLng rolante = new LatLng(-29.64721, -50.57048);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(rolante, 14));
+
+        Polygon polygon1 = googleMap.addPolygon(new PolygonOptions()
+                .add(
+                        new LatLng(-29.661293, -50.593473),
+                        new LatLng(-29.656451, -50.592503),
+                        new LatLng(-29.656646, -50.591259),
+                        new LatLng(-29.661374, -50.592114)
+                )
+        );
+
+        Polygon polygon2 = googleMap.addPolygon(new PolygonOptions()
+                .add(
+                        new LatLng(-29.656451, -50.592503),
+                        new LatLng(-29.656346, -50.593467),
+                        new LatLng(-29.657219, -50.593683),
+                        new LatLng(-29.657334, -50.592755)
+                )
+        );
+        polygon1.setTag("pol1");
+        polygon2.setTag("pol2");
+        stylePolygon(polygon1);
+        stylePolygon(polygon2);
+
+    }
+
+    private static final int COLOR_WHITE_ARGB = 0xffffffff;
+    private static final int COLOR_DARK_GREEN_ARGB = 0xff388E3C;
+    private static final int COLOR_LIGHT_GREEN_ARGB = 0x8000ff00;
+    private static final int COLOR_DARK_ORANGE_ARGB = 0xffF57F17;
+    private static final int COLOR_LIGHT_ORANGE_ARGB = 0x80FFA500;
+    private static final int COLOR_BLACK_ARGB = 0xff000000;
+
+
+    private static final int POLYGON_STROKE_WIDTH_PX = 1;
+    private static final int PATTERN_DASH_LENGTH_PX = 20;
+    private static final int PATTERN_GAP_LENGTH_PX = 20;
+
+    private static final PatternItem DASH = new Dash(PATTERN_DASH_LENGTH_PX);
+    private static final PatternItem DOT = new Dot();
+    private static final PatternItem GAP = new Gap(PATTERN_GAP_LENGTH_PX);
+
+    // Create a stroke pattern of a gap followed by a dash.
+    private static final List<PatternItem> PATTERN_POLYGON_ALPHA = Arrays.asList(GAP, DASH);
+
+    // Create a stroke pattern of a dot followed by a gap, a dash, and another gap.
+    private static final List<PatternItem> PATTERN_POLYGON_BETA =
+            Arrays.asList(DOT, GAP, DASH, GAP);
+
+    /**
+     * Styles the polygon, based on type.
+     * @param polygon The polygon object that needs styling.
+     */
+    private void stylePolygon(Polygon polygon) {
+        String type = "";
+        // Get the data object stored with the polygon.
+        if (polygon.getTag() != null) {
+            type = polygon.getTag().toString();
+        }
+
+        List<PatternItem> pattern = null;
+        int strokeColor = COLOR_BLACK_ARGB;
+        int fillColor = COLOR_WHITE_ARGB;
+
+        switch (type) {
+            // If no type is given, allow the API to use the default.
+            case "pol1":
+                // Apply a stroke pattern to render a dashed line, and define colors.
+                pattern = PATTERN_POLYGON_ALPHA;
+                strokeColor = COLOR_DARK_GREEN_ARGB;
+                fillColor = COLOR_LIGHT_GREEN_ARGB;
+                break;
+            case "pol2":
+                // Apply a stroke pattern to render a line of dots and dashes, and define colors.
+                pattern = PATTERN_POLYGON_BETA;
+                strokeColor = COLOR_DARK_ORANGE_ARGB;
+                fillColor = COLOR_LIGHT_ORANGE_ARGB;
+                break;
+        }
+
+        //polygon.setStrokePattern(pattern);
+        polygon.setStrokeWidth(POLYGON_STROKE_WIDTH_PX);
+        polygon.setStrokeColor(strokeColor);
+        polygon.setFillColor(fillColor);
     }
 }
